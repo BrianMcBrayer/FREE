cf9f12e521a6b1dc36bf3a28eed1a2cc68409e76
BrianMcBrayer <brandmcbrayer@live.com>
2015-02-24 09:57:28 -0500

    Fleshed out FREE

--- src/free.js
+++ src/free.js
@@ -2,53 +2,96 @@
 HEADER STUFF HERE
 */
 
-window.FREE = (function(my) {
+window.FREE = (function(my, ajax) {
+  'use strict';
 
-  var _data,
-      _curTestName;
+  if (ajax == null) {
+    throw new Error('promisejs is undefined or null');
+  }
 
-  my.results = {};
+  var settings = {
+    url: null,
+    data: null
+  };
+
+  var _currentBattery = null,
+      _currentTest = null;
 
   my.beginBattery = beginBattery;
   my.endBattery = endBattery;
-
   my.beginTest = beginTest;
   my.endTest = endTest;
-
-  my.mark = mark;
-
+  my.markCurrentTest = markCurrentTest;
   my.sendResults = sendResults;
 
-  function beginBattery(batteryName, data) {
-    throw new Error('beginBattery not yet implemented');
+  function beginBattery(batteryName, url, data) {
+    _currentBattery = new Battery(batteryName);
+
+    settings.url = url;
+    settings.data = data;
   }
   function endBattery() {
-    throw new Error('endBattery not yet implemented');
+    _currentBattery.complete();
+
+    sendResults(settings.url);
   }
 
   function beginTest(testName) {
-    if (my.results[testName] == null) {
+    if (my.results[testName] != null) {
       throw new Error('Test of name ' + testName + ' has already been used');
     }
 
-    my.results[testName] = [];
-
-    _curTestName = testName;
+    _currentTest = batteryResults.results[testName] = new Test(testName);
   }
   function endTest() {
-    mark(_curTestName);
-
-    
-    _curTestName = null;
+    markCurrentTest();
+    _currentTest.complete();
+    _currentTest = null;
   }
 
-  function mark(testName) {
-    throw new Error('mark not yet implemented');
+  function markCurrentTest() {
+    _currentTest.addMark();
   }
 
   function sendResults(url) {
     throw new Error('sendResults not yet implemented');
   }
 
+  function Battery(name) {
+    this.name = name;
+    this.tests = [];
+    this.start = new Date();
+    this.end = null;
+
+    this.completed: false;
+  }
+
+  Battery.prototype.complete = complete;
+  function complete() {
+    this.completed = true;
+    this.end = new Date();
+  }
+
+  function Test(name) {
+    this.name = name;
+    this.marks = [];
+
+    this.completed = false;
+  }
+
+  Test.prototype.addMark = addMark;
+  function addMark() {
+    this.marks.push(new Mark());
+  }
+
+  Test.prototype.complete = completeTest;
+  function completeTest() {
+    this.completed = true;
+  }
+
+  function Mark() {
+    this.time = new Date();
+  }
+
   return my;
-})(window.FREE || {})
+})(window.FREE || {}, promise)
